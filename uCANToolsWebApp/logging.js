5cdb520065ab36686613d5e99eb5fe5d631d0dfc
LJ <juranek.lukasz@gmail.com>
2017-06-07 07:37:51 +0200

    Logging fix

--- uCANToolsWebApp/logging.js
+++ uCANToolsWebApp/logging.js
@@ -1,24 +1,14 @@
 var fs = require('fs');
 var os = require("os");
 var config = require('./../config.json');
+var moment = require('moment');
 
 console.logCopy = console.log.bind(console);
 
-function pad(n, width, z) {
-  z = z || '0';
-  n = n + '';
-  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
-
 console.log = function(data)
 {
-      var d = new Date();
-      var currentDate = (d.toLocaleTimeString('pl-PL')) + "." + pad(d.getMilliseconds(),4) +':';
+      var currentDate = moment().format("hh:mm:ss.SSS");	
       fs.appendFileSync(config.CANLogDir +  config.CANLogPath, currentDate + data + os.EOL);
-/*    fs.appendFile(config.CANLogDir +  config.CANLogPath, currentDate + data + os.EOL, function(err){
-      if (err != null)
-        console.logCopy("Append err " + err);
-    });*/
-    console.logCopy(currentDate + data);
+      console.logCopy(currentDate +  data);
 };
 
